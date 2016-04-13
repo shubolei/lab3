9309af30733d6423b2e55cf67f3229c55b7f3e91
shlei <shlei@ucsd.edu>
2016-04-13 10:50:01 -0700

    message

---
+++ app/src/androidTest/java/test/JUnit_test.java
@@ -0,0 +1,26 @@
+package test;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.shlei.lab3.MainActivity;
+import com.example.shlei.lab3.R;
+
+import junit.framework.Test;
+
+/**
+ * Created by shlei on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+    MainActivity mainActivity;
+    public JUnit_test(){
+            super(MainActivity.class);
+    }
+    public void test_first(){
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World!",tester);
+    }
+}
